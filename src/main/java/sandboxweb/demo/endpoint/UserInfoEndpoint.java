e9d616f322ea168af27eb4efb16f542141c48185
Yoshihiro4218 <nana.mizuki77@icloud.com>
2019-12-21 17:10:15 +0900

    practice

--- src/main/java/sandboxweb/demo/endpoint/UserInfoEndpoint.java
+++ src/main/java/sandboxweb/demo/endpoint/UserInfoEndpoint.java
@@ -2,17 +2,31 @@ package sandboxweb.demo.endpoint;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
+@Slf4j
 public class UserInfoEndpoint {
 
     @GetMapping("/userinfo")
-    public UserInfoResponse userInfo() {
+    public UserInfoResponse userInfo(HttpServletRequest request,
+                                     HttpServletResponse response) {
+        Cookie requestCookie1 = request.getCookies()[0];
+        Cookie requestCookie2 = request.getCookies()[1];
+        log.info("[INFO]Cookie={}", requestCookie1.getName() + ":" + requestCookie1.getValue());
+        log.info("[INFO]Cookie={}", requestCookie2.getName() + ":" + requestCookie2.getValue());
         UserInfoResponse res = new UserInfoResponse(177L, "FukuokaTaro", 25, "windows");
+        Cookie cookie = new Cookie("OPBS", "FukuokaFinancialGroup");
+        cookie.setMaxAge(100);
+        response.addCookie(cookie);
         return res;
     }
 
